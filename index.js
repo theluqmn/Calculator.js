95841ebaf5ba74fc69b5096fe1ecd966a6387296
luqmanity <luqman.yazrie@gmail.com>
2024-02-18 16:00:37 +0800

    working

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
 const prompt = require('prompt-sync')();
 
+/*
+Number.parseFloat() is required since JavaScript treats inputs as strings, hence needs to be converted to
+float first for the arithmetic to be done correctly.
 
+Note that JavaScript is a dynamically-typed programming language.
+*/
 
 {
-    var num1 = Number.parseInt(prompt("Enter first number: "));
-    var num2 = Number.parseInt(prompt("Enter second number: "));
-    var operation = prompt("Enter an operation [+-/*]: ");
+    var num1 = Number.parseFloat(prompt("Enter first number: "));
+    var num2 = Number.parseFloat(prompt("Enter second number: "));
+    var operation = prompt("Enter an operation (+-/*%): ");
 
-    if (operation === "+") {
+    // === means strict equal
+    if (operation === "+") { //Addition
         ans = num1 + num2;
-        console.log("Addition");
+        console.log("   Addition");
 
-    } else if (operation === "-") {
+    } else if (operation === "-") { //Subtraction
         ans = num1 - num2;
-        console.log("Subtraction");
+        console.log("   Subtraction");
 
-
-    } else if (operation === "/") {
+    } else if (operation === "/") { //Division
         ans = num1 / num2;
-        console.log("Division");
+        console.log("   Division");
 
-    } else if (operation === "*") {
+    } else if (operation === "*") { //Multiplication
         ans = num1 * num2;
-        console.log("Multiplication");
+        console.log("   Multiplication");
 
-    } else {
+    } else if (operation === "%") {
+        ans = num1 % num2;
+        console.log("   Modulus");
+
+    } else { //Input for operation is none of the above
         console.log(`No operation "${operation}"!`);
         operation = "error"
     }
 
-    if (operation !== "error") {
-        console.log(`${num1} ${operation} ${num2} = ${ans}`)
+    if (operation !== "error") { //Outputs the answer
+        console.log(`Answer: ${num1} ${operation} ${num2} = ${ans}`)
     };
 }
